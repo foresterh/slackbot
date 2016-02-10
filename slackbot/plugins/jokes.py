6b50a3f0bc9477a96f29a1f2f45ed9176d43f46a
Jamie Houston <foresterh@gmail.com>
2016-02-09 21:21:53 -0800

    fixed jokes

--- slackbot/plugins/jokes.py
+++ slackbot/plugins/jokes.py
@@ -4,5 +4,13 @@ import re
 
 @respond_to('joke')
 def joke(message):
-    joke_body = utils.get_json("http://webknox.com/api/jokes/random")
-    message.reply(joke_body["joke"])
+    url = "https://webknox-jokes.p.mashape.com/jokes/search?&keywords=" + bot_input.input_string + "&minRating=5&numJokes=1"
+    headers={
+        "X-Mashape-Key": "LM6TApHaSPmshsDRI2S9h2W5eYvNp1Iin3Vjsn2ZncHbuAbsHt",
+        "Accept": "application/json"
+      }
+    story = web.get_json_with_headers(url, headers)
+    if story and len(story):
+        message.reply(story[0]["joke"])
+    else:
+        message.send("Couldn't find a joke or you're out of free jokes for the day (limit 5)")
